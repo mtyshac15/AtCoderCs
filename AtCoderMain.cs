2e38421d6b773bacb3d01531e6bf92a3efed8ea6
mtyshac15 <syktmtyshdrgn15@gmail.com>
2021-06-29 00:26:23 +0900

    典型 3

--- AtCoderMain.cs
+++ AtCoderMain.cs
@@ -38,7 +38,7 @@ public class AtCoderMain
         Action method = null;
         Typical.Problem.TypicalProblemBase problem;
 
-        problem = new Typical.Problem055.Problem();
+        problem = new Typical.Problem003.Problem();
 
         method = problem.Solve;
 
--- Typical.Problem/TypicalProblem.cs
+++ Typical.Problem/TypicalProblem.cs
@@ -15,35 +15,82 @@ public class Problem : ProblemBase
 
     public override void Solve()
     {
-        var (N, P, Q) = IOLibrary.ReadInt3();
-        var A = IOLibrary.ReadIntArray();
+        var N = IOLibrary.ReadInt();
 
-        ModInt.Init(P);
-        var modIntA = A.Select(a => (ModInt)a).ToArray();
-
-        var count = 0L;
+        //隣接リストを作成
+        var graph = new List<int>[N];
         for (int i = 0; i < N; i++)
         {
-            for (int j = 0; j < i; j++)
+            graph[i] = new List<int>();
+        }
+
+        for (int i = 0; i < N - 1; i++)
+        {
+            var (A, B) = IOLibrary.ReadInt2();
+            graph[A - 1].Add(B - 1);
+            graph[B - 1].Add(A - 1);
+        }
+
+        //グラフから頂点sを一つ選ぶ
+        int s = 0;
+        for (int i = 0; i < N; i++)
+        {
+            if (graph[i].Count == 1)
             {
-                for (int k = 0; k < j; k++)
-                {
-                    for (int l = 0; l < k; l++)
-                    {
-                        for (int m = 0; m < l; m++)
-                        {
-                            var product = modIntA[i] * modIntA[j] * modIntA[k] * modIntA[l] * modIntA[m];
-                            if (product == Q)
-                            {
-                                count++;
-                            }
-                        }
-                    }
-                }
+                s = i;
+                break;
             }
         }
 
-        IOLibrary.WriteLine(count);
+        //sから距離が最大の頂点をvを算出
+        var lengthArray = new int[N];
+        var seen = new bool[N];
+        this.Dfs(graph, s, lengthArray, seen);
+        var v = this.GetFarthestVertex(s, lengthArray);
+
+        //vからもっとも離れた点の距離を算出
+        lengthArray = new int[N];
+        seen = new bool[N];
+        this.Dfs(graph, v, lengthArray, seen);
+        var maxLength = lengthArray.Max();
+        var ans = maxLength + 1;
+        IOLibrary.WriteLine(ans);
+    }
+
+    private void Dfs(IList<IList<int>> graph, int v, int[] length, bool[] seen)
+    {
+        //vを探索済み
+        seen[v] = true;
+
+        //vに隣接しているノードを取得
+        foreach (var nextV in graph[v])
+        {
+            if (!seen[nextV])
+            {
+                //探索済みでなかったら、距離を加算
+                length[nextV] = length[v] + 1;
+                this.Dfs(graph, nextV, length, seen);
+            }
+        }
+    }
+
+    private int GetFarthestVertex(int v, int[] lengthArray)
+    {
+        var ans = v;
+
+        //vから距離が最大のノードを取得
+        var maxLength = 0;
+        for (int i = 0; i < lengthArray.Length; i++)
+        {
+            var length = lengthArray[i];
+            if (length > maxLength)
+            {
+                maxLength = length;
+                ans = i;
+            }
+        }
+
+        return ans;
     }
 }
 
