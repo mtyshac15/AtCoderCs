a0b5d669640dc3e9c014292a97992d4b1a7db78d
mtyshac15 <syktmtyshdrgn15@gmail.com>
2024-05-06 03:20:37 +0900

    ABC 334

--- Contest/ABC/300_399/330_339/334/Problem334C.cs
+++ Contest/ABC/300_399/330_339/334/Problem334C.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Intrinsics.X86;
 using System.Text;
 
 namespace AtCoderCs.Contest.ABC334;
@@ -30,9 +31,59 @@ public class ProblemC
 
     public void Solve()
     {
-        var S = _reader.ReadLine().Trim();
+        var NK = _reader.ReadLine().Trim().Split().Select(int.Parse).ToArray();
+        var N = NK[0];
+        var K = NK[1];
 
-        var ans = 0;
+        var A = _reader.ReadLine().Trim().Split().Select(long.Parse).ToArray();
+
+        //階差数列
+        var B = A.Skip(1).Zip(A, (b1, b2) => b1 - b2)
+                         .ToArray();
+
+        //階差数列の添え字が偶数の累積和
+        var sumBEven = new List<long>() { 0 };
+
+        //階差数列の添え字が奇数の累積和
+        var sumBOdd = new List<long>() { 0 };
+
+        for (var i = 0; i < K - 1; i++)
+        {
+            if (i % 2 == 0)
+            {
+                var last = sumBEven[sumBEven.Count - 1];
+                sumBEven.Add(last + B[i]);
+            }
+            else
+            {
+                var last = sumBOdd[sumBOdd.Count - 1];
+                sumBOdd.Add(last + B[i]);
+            }
+        }
+
+        var ans = 0L;
+        if (K % 2 == 0)
+        {
+            //階差数列の添え字が偶数の和
+            ans = sumBEven[sumBEven.Count - 1];
+        }
+        else
+        {
+            ans = long.MaxValue;
+
+            //階差数列の添え字が偶数の要素を除外
+            for (int i = 0; i < K; i += 2)
+            {
+                //除外した数より前の数値は添え字が偶数
+                var sum = sumBEven[i / 2];
+
+                //除外した数より後の数値は添え字が奇数
+                sum += sumBOdd[sumBOdd.Count - 1] - sumBOdd[(i + 1) / 2];
+
+                ans = Math.Min(sum, ans);
+            }
+        }
+
         _writer.WriteLine(ans);
     }
 }
