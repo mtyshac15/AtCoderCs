6e222c06a38d818120af09dbffcc46c8499dc148
mtyshac15 <syktmtyshdrgn15@gmail.com>
2024-08-08 01:12:00 +0900

    add: ABC 365

---
+++ Contest/ABC/300_399/360_369/365/Problem365D.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace AtCoderCs.Contest.ABC365;
+
+public class ProblemD
+{
+    private Reader _reader;
+    private Writer _writer;
+
+    public static void Main(string[] args)
+    {
+        Console.SetOut(new StreamWriter(Console.OpenStandardOutput()) { AutoFlush = false });
+        var problem = new ProblemD();
+        problem.Solve();
+        Console.Out.Flush();
+    }
+
+    public ProblemD()
+        : this(Console.In, Console.Out)
+    {
+    }
+
+    public ProblemD(TextReader textReader, TextWriter textWriter)
+    {
+        _reader = new Reader(textReader);
+        _writer = new Writer(textWriter);
+    }
+
+    public void Solve()
+    {
+        var N = _reader.NextInt();
+        var S = _reader.Next();
+
+        var win = 0;
+        var prev = char.MinValue;
+        var continuousCount = 1;
+        var startIndex = 0;
+
+        for (int i = 0; i < N; i++)
+        {
+            var hand = S[i];
+            if (prev != hand)
+            {
+                if (continuousCount == 1)
+                {
+                    win++;
+                }
+                else
+                {
+                    if (hand == 'R' && S[startIndex - 1] == 'P'
+                        || hand == 'S' && S[startIndex - 1] == 'R'
+                        || hand == 'P' && S[startIndex - 1] == 'S')
+                    {
+                        win += (continuousCount - 1) / 2;
+                    }
+                    else
+                    {
+                        win += continuousCount / 2;
+                    }
+                }
+
+                continuousCount = 1;
+            }
+            else
+            {
+                startIndex = i;
+                continuousCount++;
+            }
+
+            prev = hand;
+        }
+
+        if (continuousCount > 1)
+        {
+            win += (continuousCount - 1) / 2;
+        }
+
+        var ans = win;
+        _writer.WriteLine(ans);
+    }
+
+    #region "IO"
+    public class Reader
+    {
+        private TextReader _reader;
+        private int _index;
+        private string[] _line;
+
+        private char[] _cs = new char[] { ' ' };
+
+        public Reader(TextReader reader)
+        {
+            _reader = reader;
+            _index = 0;
+            _line = new string[0];
+        }
+
+        private string NextLine()
+        {
+            return _reader.ReadLine().Trim();
+        }
+
+        public string Next()
+        {
+            if (_index < _line.Length)
+            {
+                return _line[_index++];
+            }
+
+            _line = this.NextArray();
+            if (!_line.Any())
+            {
+                return this.Next();
+            }
+
+            _index = 0;
+            return _line[_index++];
+        }
+
+        public int NextInt()
+        {
+            return int.Parse(this.Next());
+        }
+
+        public long NextLong()
+        {
+            return long.Parse(this.Next());
+        }
+
+        public string[] NextArray()
+        {
+            return this.NextLine().Split(_cs, StringSplitOptions.RemoveEmptyEntries);
+        }
+
+        public int[] NextIntArray()
+        {
+            return this.NextArray().Select(int.Parse).ToArray();
+        }
+    }
+
+    class Writer
+    {
+        private TextWriter _writer;
+
+        public Writer(TextWriter writer)
+        {
+            _writer = writer;
+        }
+
+        public void WriteLine(object value = null)
+        {
+            _writer.WriteLine(value);
+        }
+
+        public void WriteYesOrNo(bool value)
+        {
+            this.WriteLine(Writer.ToYesOrNo(value));
+        }
+
+        public static string ToYesOrNo(bool value)
+        {
+            return value ? $"Yes" : $"No";
+        }
+    }
+    #endregion
+}
