2475167dbd5aa17309e7b2325bf1d14efac56d04
mtyshac15 <syktmtyshdrgn15@gmail.com>
2024-10-22 00:37:57 +0900

    add: ABC 146

---
+++ Contest.Tests/ABC/100_199/140_149/Problem146.cs
@@ -0,0 +1,38 @@
+using AtCoderCs.Common.Library;
+using AtCoderCs.Contest.ABC146;
+using AtCoderCs.Contest.Tests;
+using Contest.Tests.Modules;
+using System.Reflection;
+using Xunit;
+
+namespace AtCoderCs.Contest.Tests.ABC146;
+
+[Contest($"ABC", $"146")]
+public class Problem : IClassFixture<TestFixture>
+{
+    private static readonly ContestAttribute _attribute = Attribute.GetCustomAttribute(typeof(Problem), typeof(ContestAttribute)) as ContestAttribute;
+    private static readonly string _problemFolder = Path.Combine($"100_199", "140_149");
+
+    private TestFixture _fixture;
+
+    public Problem(TestFixture fixture)
+    {
+        _fixture = fixture;
+        _fixture.ConfigureSampleFolder(_attribute.Section, _problemFolder, _attribute.Number);
+    }
+
+#if true
+    [Theory(DisplayName = $"ABC 146")]
+    [InlineData($"A", typeof(ProblemA), nameof(ProblemA.Solve))]
+    //[InlineData($"B", typeof(ProblemB), nameof(ProblemB.Solve))]
+    //[InlineData($"C", typeof(ProblemC), nameof(ProblemC.Solve))]
+    //[InlineData($"D", typeof(ProblemD), nameof(ProblemD.Solve))]
+    //[InlineData($"E", typeof(ProblemE), nameof(ProblemE.Solve))]
+    //[InlineData($"F", typeof(ProblemF), nameof(ProblemF.Solve))]
+    public void Solve(string level, Type problemType, string methodName)
+    {
+        var sample = _fixture.ReadFiles(_attribute.Number, level);
+        TestTools.Solve(sample, problemType, methodName);
+    }
+#endif
+}
