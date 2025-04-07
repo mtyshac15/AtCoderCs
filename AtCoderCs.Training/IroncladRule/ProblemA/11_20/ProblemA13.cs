84102237455bc4a86f181c440e53d9cea5777714
mtyshac15 <syktmtyshdrgn15@gmail.com>
2025-04-08 00:23:51 +0900

    鉄則 A13

---
+++ AtCoderCs.Training/IroncladRule/ProblemA/11_20/ProblemA13.cs
@@ -0,0 +1,166 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace AtCoderCs.Training.A13;
+
+public class ProblemA
+{
+    private Reader _reader;
+    private Writer _writer;
+
+    public ProblemA(TextReader textReader, TextWriter textWriter)
+    {
+        _reader = new Reader(textReader);
+        _writer = new Writer(textWriter);
+    }
+
+    public void Solve()
+    {
+        var N = _reader.Int();
+        var K = _reader.Int();
+        var A = _reader.IntArray();
+
+        var count = 0L;
+
+        for (int i = 0; i < N; i++)
+        {
+            Func<int, bool> judge = (mIndex) =>
+            {
+                return Math.Abs(A[mIndex] - A[i]) <= K;
+            };
+
+            var index = BinarySerch(i, A.Length, judge);
+            count += index - i;
+        }
+
+        var ans = count;
+        _writer.WriteLine(ans);
+    }
+
+    private int BinarySerch(int ok, int ng, Func<int, bool> judge)
+    {
+        while (Math.Abs(ok - ng) > 1)
+        {
+            var min = Math.Min(ok, ng);
+            var max = Math.Max(ok, ng);
+
+            var middle = min + (max - min) / 2;
+            if (judge(middle))
+            {
+                ok = middle;
+            }
+            else
+            {
+                ng = middle;
+            }
+        }
+
+        return ok;
+    }
+}
+
+#region
+class ProgramA
+{
+    public static void Main(string[] args)
+    {
+        Console.SetOut(new StreamWriter(Console.OpenStandardOutput()) { AutoFlush = false });
+        var problem = new ProblemA(Console.In, Console.Out);
+        problem.Solve();
+        Console.Out.Flush();
+    }
+}
+
+class Reader
+{
+    private TextReader _reader;
+    private int _index;
+    private string[] _line;
+
+    private char[] _cs = new char[] { ' ' };
+
+    public Reader(TextReader reader)
+    {
+        _reader = reader;
+        _index = 0;
+        _line = Array.Empty<string>();
+    }
+
+    private string NextLine()
+    {
+        var line = _reader.ReadLine() ?? string.Empty;
+        return line.Trim();
+    }
+
+    public string Str()
+    {
+        if (_index < _line.Length)
+        {
+            return _line[_index++];
+        }
+
+        _line = this.StrArray();
+        if (!_line.Any())
+        {
+            return this.Str();
+        }
+
+        _index = 0;
+        return _line[_index++];
+    }
+
+    public int Int()
+    {
+        return int.Parse(this.Str());
+    }
+
+    public long Long()
+    {
+        return long.Parse(this.Str());
+    }
+
+    public string[] StrArray()
+    {
+        return this.NextLine().Split(_cs, StringSplitOptions.RemoveEmptyEntries);
+    }
+
+    public int[] IntArray()
+    {
+        return this.StrArray().Select(int.Parse).ToArray();
+    }
+
+    public long[] LongArray()
+    {
+        return this.StrArray().Select(long.Parse).ToArray();
+    }
+}
+
+class Writer
+{
+    private TextWriter _writer;
+
+    public Writer(TextWriter writer)
+    {
+        _writer = writer;
+    }
+
+    public void WriteLine(object? value = null)
+    {
+        _writer.WriteLine(value);
+    }
+
+    public void WriteYesOrNo(bool value)
+    {
+        this.WriteLine(Writer.ToYesOrNo(value));
+    }
+
+    public static string ToYesOrNo(bool value)
+    {
+        return value ? $"Yes" : $"No";
+    }
+}
+#endregion
+
