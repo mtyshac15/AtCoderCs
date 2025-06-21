85b1881a56f9b9b87d19c21b11c4b927aba52ff2
mtyshac15 <syktmtyshdrgn15@gmail.com>
2025-06-21 23:33:38 +0900

    鉄則

---
+++ AtCoderCs.Training/IroncladRule/ProblemA/11_20/ProblemA16.cs
@@ -0,0 +1,140 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace AtCoderCs.Training.A16;
+
+public class ProblemA
+{
+  private Reader _reader;
+  private Writer _writer;
+
+  public ProblemA(TextReader textReader, TextWriter textWriter)
+  {
+    _reader = new Reader(textReader);
+    _writer = new Writer(textWriter);
+  }
+
+  public void Solve()
+  {
+    var N = _reader.Int();
+    var A = _reader.IntArray();
+    var B = _reader.IntArray();
+
+    var dp = new int[N];
+    dp[0] = 0;
+    dp[1] = A[0];
+
+    for (int i = 2; i < N; i++)
+    {
+      dp[i] = Math.Min(dp[i - 1] + A[i - 1], dp[i - 2] + B[i - 2]);
+    }
+
+    var ans = dp[N - 1];
+    _writer.WriteLine(ans);
+  }
+}
+
+#region
+class ProgramA
+{
+  public static void Main(string[] args)
+  {
+    Console.SetOut(new StreamWriter(Console.OpenStandardOutput()) { AutoFlush = false });
+    var problem = new ProblemA(Console.In, Console.Out);
+    problem.Solve();
+    Console.Out.Flush();
+  }
+}
+
+class Reader
+{
+  private TextReader _reader;
+  private int _index;
+  private string[] _line;
+
+  private char[] _cs = new char[] { ' ' };
+
+  public Reader(TextReader reader)
+  {
+    _reader = reader;
+    _index = 0;
+    _line = Array.Empty<string>();
+  }
+
+  private string NextLine()
+  {
+    var line = _reader.ReadLine() ?? string.Empty;
+    return line.Trim();
+  }
+
+  public string Str()
+  {
+    if (_index < _line.Length)
+    {
+      return _line[_index++];
+    }
+
+    _line = this.StrArray();
+    if (!_line.Any())
+    {
+      return this.Str();
+    }
+
+    _index = 0;
+    return _line[_index++];
+  }
+
+  public int Int()
+  {
+    return int.Parse(this.Str());
+  }
+
+  public long Long()
+  {
+    return long.Parse(this.Str());
+  }
+
+  public string[] StrArray()
+  {
+    return this.NextLine().Split(_cs, StringSplitOptions.RemoveEmptyEntries);
+  }
+
+  public int[] IntArray()
+  {
+    return this.StrArray().Select(int.Parse).ToArray();
+  }
+
+  public long[] LongArray()
+  {
+    return this.StrArray().Select(long.Parse).ToArray();
+  }
+}
+
+class Writer
+{
+  private TextWriter _writer;
+
+  public Writer(TextWriter writer)
+  {
+    _writer = writer;
+  }
+
+  public void WriteLine(object? value = null)
+  {
+    _writer.WriteLine(value);
+  }
+
+  public void WriteYesOrNo(bool value)
+  {
+    this.WriteLine(Writer.ToYesOrNo(value));
+  }
+
+  public static string ToYesOrNo(bool value)
+  {
+    return value ? $"Yes" : $"No";
+  }
+}
+#endregion
---
+++ AtCoderCsTest.Training.Tests/IroncladRule/ProblemA/18/TestProblem18.cs
@@ -0,0 +1,39 @@
+using AtCoderCs.Training.A18;
+using AtCoderCsTest.Contents.Services;
+using AtCoderCsTest.Training.Tests.Services;
+using Microsoft.Extensions.Logging;
+using System.Reflection;
+using Xunit.Abstractions;
+
+namespace AtCoderCs.Training.Tests.A18;
+
+[Collection(nameof(TrainingTestCollection))]
+[Contest($"IroncladRule", $"18")]
+[Sample($"IroncladRule", $"ProblemA", $"18")]
+public class TestProblem
+{
+  private static readonly ContestAttribute _contestAttribute = typeof(TestProblem).GetCustomAttribute<ContestAttribute>()!;
+  private static readonly SampleAttribute _sampleAttribute = typeof(TestProblem).GetCustomAttribute<SampleAttribute>()!;
+
+  private ILogger _logger;
+  private ISampleRepository _sampleRepository;
+  private TestJudgeService _judgeService;
+
+  public TestProblem(ITestOutputHelper output, TrainingTestFixture fixture)
+  {
+    _logger = new XunitLogger(output);
+    _sampleRepository = fixture.GetSampleRepository(_sampleAttribute.Directory);
+    _judgeService = new TestJudgeService(_logger);
+  }
+
+#if DEBUG
+  [Theory(DisplayName = $"IroncladRule 18")]
+  [InlineData($"", typeof(ProblemA), nameof(ProblemA.Solve))]
+  public void Solve(string level, Type problemType, string methodName, int radix = 10, int exp = 0)
+  {
+    var sample = _sampleRepository.Find(_contestAttribute.Number, level);
+    var results = _judgeService.Solve(sample, problemType, methodName);
+    _judgeService.Judge(results, radix, exp);
+  }
+#endif
+}
