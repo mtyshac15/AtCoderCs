16f84658bf74b290057772763de4575926805293
mtyshac15 <syktmtyshdrgn15@gmail.com>
2025-06-30 23:41:52 +0900

    add: ABC 397

---
+++ AtCoderCs.Contest/ABC/300_399/390_399/397/Problem397A.cs
@@ -0,0 +1,150 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace AtCoderCs.Contest.ABC397.ProblemA;
+
+public class ProblemA
+{
+  private Reader _reader;
+  private Writer _writer;
+
+  public ProblemA(TextReader textReader, TextWriter textWriter)
+  {
+    _reader = new Reader(textReader);
+    _writer = new Writer(textWriter);
+  }
+
+  public void Solve()
+  {
+    var X = decimal.Parse(_reader.Str());
+
+    var t = X switch
+    {
+      >= 38.0m => 1,
+      >= 37.5m and < 38.0m => 2,
+      < 37.5m => 3,
+    };
+
+    var ans = t;
+    _writer.WriteLine(ans);
+  }
+}
+
+#region
+class ProgramA
+{
+  public static void Main(string[] args)
+  {
+    Console.SetOut(new StreamWriter(Console.OpenStandardOutput()) { AutoFlush = false });
+    var problem = new ProblemA(Console.In, Console.Out);
+    problem.Solve();
+    Console.Out.Flush();
+  }
+}
+
+class Reader
+{
+  private TextReader _reader;
+  private int _index;
+  private string[] _line;
+
+  private char[] _cs = new char[] { ' ' };
+
+  public Reader(TextReader reader)
+  {
+    _reader = reader;
+    _index = 0;
+    _line = Array.Empty<string>();
+  }
+
+  private string NextLine()
+  {
+    var line = _reader.ReadLine() ?? string.Empty;
+    return line.Trim();
+  }
+
+  public string Str()
+  {
+    if (_index < _line.Length)
+    {
+      return _line[_index++];
+    }
+
+    _line = this.StrArray();
+    if (!_line.Any())
+    {
+      return this.Str();
+    }
+
+    _index = 0;
+    return _line[_index++];
+  }
+
+  public int Int()
+  {
+    return int.Parse(this.Str());
+  }
+
+  public long Long()
+  {
+    return long.Parse(this.Str());
+  }
+
+  public string[] StrArray()
+  {
+    return this.NextLine().Split(_cs, StringSplitOptions.RemoveEmptyEntries);
+  }
+
+  public int[] IntArray()
+  {
+    return this.StrArray().Select(int.Parse).ToArray();
+  }
+
+  public long[] LongArray()
+  {
+    return this.StrArray().Select(long.Parse).ToArray();
+  }
+
+  public char[,] Grid(int H, int W)
+  {
+    var grid = new char[H, W];
+    for (int h = 0; h < H; h++)
+    {
+      var line = Str();
+      for (int w = 0; w < W; w++)
+      {
+        grid[h, w] = line[w];
+      }
+    }
+    return grid;
+  }
+}
+
+class Writer
+{
+  private TextWriter _writer;
+
+  public Writer(TextWriter writer)
+  {
+    _writer = writer;
+  }
+
+  public void WriteLine(object? value = null)
+  {
+    _writer.WriteLine(value);
+  }
+
+  public void WriteYesOrNo(bool value)
+  {
+    this.WriteLine(Writer.ToYesOrNo(value));
+  }
+
+  public static string ToYesOrNo(bool value)
+  {
+    return value ? $"Yes" : $"No";
+  }
+}
+#endregion
